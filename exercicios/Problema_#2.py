3d89d517ecbd2c39be0118c221ad17f15064d3b3
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-08-13 17:20:54 +0100

    Adicionado e resolvido, Problema 1 e 2

---
+++ exercicios/Problema_#2.py
@@ -0,0 +1,9 @@
+""" Faça um programa que pergunte o nome e a idade do usuário e o exiba na tela.
+
+“Bem vindo <nome>, fico feliz em saber que tem <idade> anos” """
+
+nome = input('Qual é o seu nome mano? ')
+
+idade = int(input('Diz aí a sua idade? '))
+
+print('\n'f'Bem vindo {nome}, fico feliz em saber que tem {idade} anos')
