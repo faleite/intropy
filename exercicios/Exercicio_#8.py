cef324ef1a40c5475ffa2db52bad7bae504e503c
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-08-15 18:47:30 +0100

    Incluida resolução do profesor no exercício #8

--- exercicios/Exercicio_#8.py
+++ exercicios/Exercicio_#8.py
@@ -1,8 +1,14 @@
 """ Faça um programa que receba uma data de nascimento (15/07/87) e imprima:
 ‘Você nasceu em <dia> de <mês> de <ano>’ """
 
+# Minha Resolução
 data = input('Digite a sua data de nascimento: ')
 
 data2 = data.split('/')
 
 print(f"\n‘Você nasceu em {data2[0]} de {data2[1]} de {data2[2]}’")
+
+# Resolução do professor
+dia, mes, ano = data.split('/')
+
+print(f"\n‘Você nasceu em {dia} de {mes} de {ano}’")
