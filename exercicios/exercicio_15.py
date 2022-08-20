abcfc26840749be1dfb2f3857b1a002f6f507f0a
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-08-20 18:25:08 +0100

    Feito a resolução do exercício 15

--- exercicios/exercicio_15.py
+++ exercicios/exercicio_15.py
@@ -5,3 +5,54 @@ a combinatória de dois elementos e nos retorne as combinações em uma nova lis
 
  - Exemplo de saída: [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
 """
+
+# Essa foi a resolução que cheguei. É preciso melhorar.
+
+entrada = [1, 2, 3, 4]
+
+saida = []
+
+lista = []
+
+index = 0
+index2 = 1
+
+for n in entrada:
+    lista.append(entrada[index])
+    if index2 < len(entrada):
+        lista.append(entrada[index2])
+        saida.append(lista)
+        index2 += 1
+        lista = []
+
+entrada.pop(0)
+
+lista2 = []
+
+index = 0
+index2 = 1
+
+for n in entrada:
+    lista2.append(entrada[index])
+    if index2 < len(entrada):
+        lista2.append(entrada[index2])
+        saida.append(lista2)
+        lista2 = []
+        index2 += 1
+
+entrada.pop(0)
+
+lista2 = []
+
+index = 0
+index2 = 1
+
+for n in entrada:
+    lista2.append(entrada[index])
+    if index2 < len(entrada):
+        lista2.append(entrada[index2])
+        saida.append(lista2)
+        lista2 = []
+        index2 += 1
+
+print(saida)
