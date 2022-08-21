13a1ebb67f9a0581549e1a44daddfec55b85acad
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-08-21 14:02:34 +0100

    Feito a resolução do exercício 15

--- exercicios/exercicio_15.py
+++ exercicios/exercicio_15.py
@@ -6,53 +6,37 @@ a combinatória de dois elementos e nos retorne as combinações em uma nova lis
  - Exemplo de saída: [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
 """
 
-# Essa foi a resolução que cheguei. É preciso melhorar.
+#  from itertools import combinations
 
 entrada = [1, 2, 3, 4]
-
+tamanho = len(entrada)
 saida = []
 
-lista = []
+#3 Resolução Lelzin
+#  saida1 = list(combinations(entrada, 2))
+#  print(saida1)
 
-index = 0
-index2 = 1
+#1 Resolução Thiago Kim
+#  for n in range(tamanho):
+    #  lista = [[entrada[n], entrada[i]] for i in range(n + 1, tamanho)]
+    #  saida.extend(lista)
 
-for n in entrada:
-    lista.append(entrada[index])
-    if index2 < len(entrada):
-        lista.append(entrada[index2])
-        saida.append(lista)
-        index2 += 1
-        lista = []
+#2 Resolução Thiago Kim
+#  for n in range(tamanho):
+    #  z = zip(entrada, entrada[n+1:])
+    #  saida.extend(list(z))
 
-entrada.pop(0)
+#2.1 Resolução Thiago Kim
+#  for n, num in enumerate(entrada):
+    #  z = list(zip(entrada,entrada[n+1:]))
+    #  saida.extend([list(itm) for itm in z])
 
-lista2 = []
+#  saida.sort() # ou
+#  saida = sorted(saida)
 
-index = 0
-index2 = 1
-
-for n in entrada:
-    lista2.append(entrada[index])
-    if index2 < len(entrada):
-        lista2.append(entrada[index2])
-        saida.append(lista2)
-        lista2 = []
-        index2 += 1
-
-entrada.pop(0)
-
-lista2 = []
-
-index = 0
-index2 = 1
-
-for n in entrada:
-    lista2.append(entrada[index])
-    if index2 < len(entrada):
-        lista2.append(entrada[index2])
-        saida.append(lista2)
-        lista2 = []
-        index2 += 1
+#1.1 Meu debug sobre a resolução 1
+for n in range(tamanho):
+    for i in range(n+1, tamanho):
+        saida.append([entrada[n], entrada[i]])
 
 print(saida)
